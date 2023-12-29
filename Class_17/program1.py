b90d9742d6e47f18920addb6d39fabe6544c1136
Zoha Memon <110814392+Zoha243@users.noreply.github.com>
2023-12-30 00:48:26 +0500

    Add files via upload

---
+++ Class_17/program1.py
@@ -0,0 +1,8 @@
+# Functions program:
+
+def callingSum(x,y):
+    sum=x+y
+    return sum
+
+print(callingSum(1,2))
+
---
+++ Class_17/program2.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+
+add_numbers(3, 5)
+
+# Print the result
+print("Sum:",add_numbers(3, 5))
