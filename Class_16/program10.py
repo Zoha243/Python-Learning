3f586650c98c2fadf25cbd6ba55d8baf659d2988
Zoha Memon <110814392+Zoha243@users.noreply.github.com>
2023-12-30 00:56:00 +0500

    Add files via upload

---
+++ Class_16/program10.py
@@ -0,0 +1,19 @@
+# creating a  circle like that:
+
+"""
+
+  *****
+ *     *
+*       *
+*       *
+*       *
+ *     *
+  *****
+
+"""
+len = 6
+print(" "*1,"*"*len)
+for i in range(1):
+    print("*"+" "*(len)+"  *")
+
+print(" "*1,"*"*len)
---
+++ Class_16/program3.py
@@ -0,0 +1,10 @@
+
+#        * 
+#      * * 
+#    * * * 
+#  * * * * 
+#* * * * * 
+
+length = int(input("Enter number"))
+for i in range(1,length+1):
+    print((length-i)*" "+i*"*")
